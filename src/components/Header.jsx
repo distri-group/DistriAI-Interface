eca5d34a9e71a0dc7ef15f5ef96996aad9f382ac
lymane <lymane0121@gmail.com>
2024-01-18 14:10:57 +0800

    Add Document Entry

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,39 @@
+import { Dropdown } from "antd";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 function Header({ className }) {
   let navigate = useNavigate();
+  const items = [
+    {
+      label: (
+        <a
+          href="https://github.com/distri-group"
+          style={{ fontSize: "16px" }}
+          target="_blank"
+          rel="noreferrer">
+          Github
+        </a>
+      ),
+    },
+    {
+      label: (
+        <span style={{ fontSize: "16px", cursor: "default" }}>Whitebook</span>
+      ),
+      disabled: true,
+    },
+    {
+      label: (
+        <a
+          href="https://medium.com/@Distri.AI"
+          style={{ fontSize: "16px" }}
+          target="_blank"
+          rel="noreferrer">
+          Blog
+        </a>
+      ),
+    },
+  ];
   return (
     <div className={className}>
       <div className="con">
@@ -10,10 +41,33 @@ function Header({ className }) {
           src="/img/logo.png"
           style={{
             width: "120px",
+            height: "28px",
+            marginTop: "14px",
           }}
           alt=""
         />
-        <span onClick={() => navigate("/market/")}>Launch APP</span>
+        <div
+          style={{
+            display: "flex",
+            width: "400px",
+            justifyContent: "space-between",
+          }}>
+          <Dropdown
+            menu={{ items }}
+            placement="bottom"
+            overlayClassName="dropdown">
+            <span style={{ color: "white" }}>Resources</span>
+          </Dropdown>
+          <a
+            href="https://docs.distri.ai/core/"
+            target="_blank"
+            rel="noreferrer">
+            Docs
+          </a>
+          <span className="launch" onClick={() => navigate("/market/")}>
+            Launch APP
+          </span>
+        </div>
       </div>
     </div>
   );
@@ -24,11 +78,14 @@ export default styled(Header)`
   height: 56px;
   line-height: 56px;
   display: block;
+  a {
+    text-decoration: none;
+    color: white;
+  }
   .con {
     width: 1200px;
     display: flex;
     justify-content: space-between;
-    align-items: center;
     flex-direction: row;
     overflow: hidden;
     margin: 0 auto;
@@ -37,8 +94,9 @@ export default styled(Header)`
       margin-left: 20px;
       cursor: pointer;
     }
-    span {
+    .launch {
       margin-right: 20px;
+      margin-top: 9px;
       color: white;
       line-height: 38px;
       height: 38px;
