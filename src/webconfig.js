f7a6e3c98f0d0f0597cbf658f2ba62fb91f69eb0
lymane <lymane0121@gmail.com>
2024-01-23 11:07:02 +0800

    code format

--- src/webconfig.js
+++ src/webconfig.js
@@ -1,5 +1,4 @@
 const webconfig = {
-  videoApiUrl: "/cmps",
   apiUrl: process.env.REACT_APP_API_URL,
   contractAddress: "HF4aT6sho2zTySB8nEeN5ThMvDGtGVRrH3jeBvxFNxit",
   mintAddress: "896KfVVY6VRGQs1d9CKLnKUEgXXCCJcEEg7LwSK84vWE",
