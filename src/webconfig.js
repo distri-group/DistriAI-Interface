e3813b431fccc54211382b3867a2b2658a0807c0
lymane <lymane0121@gmail.com>
2024-04-02 20:39:41 +0800

    environment format

--- src/webconfig.js
+++ src/webconfig.js
@@ -2,10 +2,7 @@ import { PublicKey } from "@solana/web3.js";
 
 const webconfig = {
   apiUrl: process.env.REACT_APP_API_URL,
-  // mainnet
-  PROGRAM: new PublicKey("6yFTDdiS1W9T9yg6YejkwKggkEE4NYqdSSzVqQvuLn16"),
-  // testnet
-  // PROGRAM: new PublicKey("8WxFh7ZtvTP1587yvyweoRMt41hCHMsmM48PTbZKM4tY"),
+  PROGRAM: new PublicKey(process.env.REACT_APP_PROGRAM),
   MINT_PROGRAM: new PublicKey("896KfVVY6VRGQs1d9CKLnKUEgXXCCJcEEg7LwSK84vWE"),
 };
 export default webconfig;
