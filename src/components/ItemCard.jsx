6dde69aac3a50e3ac6cf41362b1a87149afeb93a
lymane <lymane0121@gmail.com>
2024-06-19 10:50:36 +0800

    model&dataset status update

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,22 +1,32 @@
 import { AccountBalance } from "@mui/icons-material";
-import { Chip, Stack } from "@mui/material";
+import { Button, Chip, Stack } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { itemPublish } from "../services/model";
 import { useAnchorWallet } from "@solana/wallet-adapter-react";
 import { useSnackbar } from "notistack";
+import { useState } from "react";
 
 function ItemCard({ item, className, type, onReload }) {
   const navigate = useNavigate();
   const wallet = useAnchorWallet();
   const { enqueueSnackbar } = useSnackbar();
+  const isExaming = item.Status === 1;
+  const isPublished = item.Status === 2;
+  const [loading, setLoading] = useState(false);
   const handlePublish = async () => {
+    setLoading(true);
     try {
-      await itemPublish(type, item.Name, !item.Public, wallet);
+      await itemPublish(type, item.Name, !isPublished, wallet);
+      enqueueSnackbar(`${isPublished ? "Unlist" : "Publish"} ${type} success`, {
+        variant: "success",
+      });
+      onReload();
     } catch (error) {
       enqueueSnackbar(error.message, { variant: "error" });
     }
+    setLoading(false);
   };
   return (
     <div className={className}>
@@ -65,13 +75,40 @@ function ItemCard({ item, className, type, onReload }) {
         </Stack>
         {onReload && (
           <>
-            <span className="item-status">In draft</span>
-            <LoadingButton
-              onClick={handlePublish}
-              className="cbtn"
-              style={{ width: 100 }}>
-              Publish
-            </LoadingButton>
+            {isPublished ? (
+              <>
+                <span className="item-status Publish">On sale</span>
+                <LoadingButton
+                  loading={loading}
+                  onClick={handlePublish}
+                  className="unlist"
+                  style={{ width: 100 }}>
+                  {!loading && "Unlist"}
+                </LoadingButton>
+              </>
+            ) : (
+              <>
+                <span className="item-status Private">In draft</span>
+                {isExaming ? (
+                  <Button className="disabled" style={{ width: 100 }} disabled>
+                    Examing
+                  </Button>
+                ) : (
+                  <>
+                    {item.Reason && (
+                      <span className="failed">{item.Reason}</span>
+                    )}
+                    <LoadingButton
+                      loading={loading}
+                      onClick={handlePublish}
+                      className="cbtn"
+                      style={{ width: 100 }}>
+                      {!loading && "Publish"}
+                    </LoadingButton>
+                  </>
+                )}
+              </>
+            )}
           </>
         )}
       </Stack>
@@ -101,8 +138,30 @@ export default styled(ItemCard)`
   }
   .item-status {
     display: block;
-    padding: 6px 36px;
+    padding: 6px 0;
+    width: 120px;
+    text-align: center;
     box-sizing: border-box;
     border: 1px solid #898989;
+  }
+  .Publish {
+    border-color: #1bce58;
+    color: #1bce58;
+  }
+  .Private {
+    border-color: #ce1ba1;
+    color: #ce1ba4;
+  }
+  .unlist {
+    height: 48px;
+    background: #ff6073;
+    color: white;
+  }
+  .disabled {
+    height: 48px;
+    background-color: rgba(70, 70, 70, 1);
+  }
+  .failed {
+    color: #ff6073;
   }
 `;
